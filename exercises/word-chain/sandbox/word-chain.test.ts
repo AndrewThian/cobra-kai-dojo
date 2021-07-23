cff2aeb5018c89da8ade9e0d00962fa0f4f13713
AndrewThian <andrew@stashaway.com>
2021-07-23 17:17:34 +0800

    session-4

--- exercises/word-chain/sandbox/word-chain.test.ts
+++ exercises/word-chain/sandbox/word-chain.test.ts
@@ -1,5 +1,21 @@
-import { word-chain } from './word-chain'
+import { wordChain } from './word-chain'
 
 it('word-chain', () => {
-  expect(word-chain()).toEqual('change me here')
+  expect(wordChain('cat', 'dog')).toEqual(['cat', 'cot', 'cog', 'dog'])
+  expect(wordChain('cat', 'fog')).toEqual(['cat', 'cot', 'cog', 'fog'])
+  expect(wordChain('cat', 'cog')).toEqual(['cat', 'cot', 'cog'])
+  expect(wordChain('cow', 'lot')).toEqual(['cow', 'low', 'lot'])
+  expect(wordChain('jag', 'hat')).toEqual(['jag', 'hag', 'hat'])
+  expect(wordChain('lead', 'gold')).toEqual(['lead', 'load', 'goad', 'gold' ])
+  // expect(wordChain('ruby', 'code')).toEqual(['ruby', 'rubs', 'robs', 'rods', 'rode', 'code'])
 })
+
+// ruby, rubs, robs, rods, rode, code
+// ruby, rubs, robs, rods, rode, code
+
+ // cuby roby rody rube
+
+
+ // ruby -> rubs
+
+ // rubs -> robs
--- exercises/word-chain/sandbox/word-chain.ts
+++ exercises/word-chain/sandbox/word-chain.ts
@@ -1,1 +1,80 @@
-export const word-chain = () => 'change me here'
+const dictionary = [
+  'cat', 'cot', 'cog', 'dog', 'fog', 'cow', 'low', 'lot', 'jag', 'hag', 'hat',
+  'lead', 'load', 'goad', 'gold', 'ruby', 'rubs', 'robs', 'rods', 'rode', 'code'
+]
+
+function isOneLetterDiff(wordOne, wordTwo) {
+  if (wordOne.length !== wordTwo.length) return false;
+
+  let count = 0;  
+  for (let i = 0; i < wordOne.length; i++) {
+    if (wordTwo[i] === wordOne[i]) count++
+  }
+  
+  return (wordOne.length - count) === 1;
+}
+
+// function recurse(current, last, dictionary) {
+//   const wordsWithOneLetterDiff = ['sat', 'cot', 'hat'];
+//   const results = wordsWithOneLetterDiff.map((word) => recurse(word, last, dictionaryMinusCurrent))
+//   const shortestResult = ...
+//   return [current, ...shortestResult];
+// }
+
+export const wordChain = (first: string, last: string) => {
+  const result = []
+
+  result.push(first)
+
+  // let current = first;
+  // let previous = current;
+
+  // while(true) {
+
+  //   for (let i = 0; i < dictionary.length; i++) {
+  //     const word = dictionary[i]
+
+  //     if (!result.includes(word) && isOneLetterDiff(current, word)) {
+  //       previous = current;
+  //       current = word;
+  //       result.push(current)
+  //       break;
+  //     }
+  //   }
+
+  //   if (current === last) break;
+  //   if (previous === current) break;
+  // }
+
+  
+  // dictionary.forEach(word => {
+  //   if (isOneLetterDiff(current, word)) {
+  //     current = word;
+  //     result.push(current)
+  //   }
+  // })
+
+  let current = first;
+  let previous = current;
+  while (true) {
+    for (let i = 0; i < current.length; i++) {
+      const charCurrent = current[i];
+      const charLast = last[i];
+      const newWord = current.split('');
+      newWord[i] = charLast;
+      const newString = newWord.join('');
+      if (newString !== current && dictionary.includes(newString)) { 
+        previous = current;
+        current = newString;
+        result.push(current);
+        break;
+      }
+    }
+
+    if (current === last) break;
+    if (previous === current) break;
+  }
+
+  return result
+}
+
