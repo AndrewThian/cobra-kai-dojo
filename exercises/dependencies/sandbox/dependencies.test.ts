16c8c7aa678e599c106ee46d4b0965339dc1654c
yingqiu <yingqiu@stashaway.com>
2021-08-06 16:44:27 +0800

    soemin, vitaliy, aloysius, chew

--- exercises/dependencies/sandbox/dependencies.test.ts
+++ exercises/dependencies/sandbox/dependencies.test.ts
@@ -1,5 +1,69 @@
-import { dependencies } from './dependencies'
+import { getDependencies } from './dependencies'
 
 it('dependencies', () => {
-  expect(dependencies()).toEqual('change me here')
+  const dependencies = {
+    'A': ['B', 'C']
+  }
+  expect(getDependencies('A', dependencies)).toEqual(['B', 'C'])
 })
+
+it('dependencies', () => {
+  const dependencies = {
+    'A': ['B'],
+    'B': ['C']
+  }
+  expect(getDependencies('A', dependencies)).toEqual(['B', 'C'])
+})
+
+it('dependencies without transitive', () => {
+  const dependencies = {
+    'A': ['B', 'D'],
+    'B': ['C']
+  }
+  expect(getDependencies('A', dependencies)).toEqual(['B', 'D', 'C'])
+})
+
+it('dependencies with transitive', () => {
+  const dependencies = {
+    'A': ['B', 'D'],
+    'B': ['C'],
+    'C': ['E']
+  }
+  expect(getDependencies('A', dependencies)).toEqual(['B', 'D', 'C', 'E'])
+})
+
+it('dependencies with orphaned dependencies', () => {
+  const dependencies = {
+    'A': ['B', 'D'],
+    'B': ['C'],
+    'C': ['E'],
+    'E': ['F'],
+    'Z': ['Y']
+  }
+  expect(getDependencies('A', dependencies)).toEqual(['B', 'D', 'C', 'E', 'F'])
+})
+
+
+it('dependencies with duplicated dependencies', () => {
+  const dependencies = {
+    'A': ['B', 'D'],
+    'B': ['C'],
+    'C': ['E'],
+    'D': ['C'],
+    'E': ['F'],
+    'Z': ['Y']
+  }
+  expect(getDependencies('A', dependencies)).toEqual(['B', 'D', 'C', 'E', 'F'])
+})
+
+
+it('dependencies with circular references', () => {
+  const dependencies = {
+    'A': ['B'],
+    'B': ['C'],
+    'C': ['A']
+  }
+  expect(getDependencies('A', dependencies)).toEqual(['B', 'C'])
+})
+
+
--- exercises/dependencies/sandbox/dependencies.ts
+++ exercises/dependencies/sandbox/dependencies.ts
@@ -1,1 +1,10 @@
-export const dependencies = () => 'change me here'
+const _ =  require('lodash');
+
+export const getDependencies = (module, tree) =>  {
+    let all_dependencies = [...tree[module]]
+    for (let i=0; i < all_dependencies.length; i++) {
+        const dependenciesToPush = _.difference(tree[all_dependencies[i]], [...all_dependencies, module]) // returns all new dependencies not in all dependencies
+        all_dependencies.push(...dependenciesToPush)
+    }
+    return all_dependencies
+}
