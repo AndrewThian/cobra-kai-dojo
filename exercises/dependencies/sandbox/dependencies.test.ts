b811cae5c7204fab9e4871415b714f20a9abf0d7
Andrew Thian <andrew@stashaway.com>
2021-08-13 14:58:23 +0800

    dependecies

--- exercises/dependencies/sandbox/dependencies.test.ts
+++ exercises/dependencies/sandbox/dependencies.test.ts
@@ -1,5 +1,46 @@
-import { dependencies } from './dependencies'
+import { Dependencies } from './dependencies'
 
-it('dependencies', () => {
-  expect(dependencies()).toEqual('change me here')
+let deps;
+
+beforeEach(() => {
+  deps = new Dependencies()
 })
+
+it('dependencies F - H', () => {
+  deps.addDirect('F', ['H']);
+  const result = deps.dependenciesFor('F');
+  expect(result).toEqual(new Set(['H']));
+})
+
+it('dependencies F - H I', () => {
+  deps.addDirect('F', ['H', 'I']);
+  const result = deps.dependenciesFor('F');
+  expect(result).toEqual(new Set(['H', 'I']));
+})
+
+it('dependencies F - H I, E - F', () => {
+  deps.addDirect('F', ['H', 'I']);
+  deps.addDirect('E', ['F'])
+
+  const result = deps.dependenciesFor('F')
+  expect(result).toEqual(new Set(['H', 'I']))
+})
+
+it('dependencies F - H I, E - F', () => {
+  deps.addDirect('F', ['H', 'I']);
+  deps.addDirect('E', ['F'])
+
+  const result = deps.dependenciesFor('E')
+  expect(result).toEqual(new Set(['F', 'H', 'I']))
+})
+
+it("multi level deps", () => {
+  deps.addDirect('E', ['F']);
+  deps.addDirect('F', ['H', 'I']);
+  deps.addDirect('H', ['X']);
+
+  const result = deps.dependenciesFor('E')
+  expect(result).toEqual(new Set(['F', 'H', 'I', 'X']))
+})
+
+it.todo("duplicate keys being sent in")
--- exercises/dependencies/sandbox/dependencies.ts
+++ exercises/dependencies/sandbox/dependencies.ts
@@ -1,1 +1,60 @@
-export const dependencies = () => 'change me here'
+export class Dependencies {
+  private adjacencyList: Map<string, Set<string>>
+  private mapFullDeps: Map<string, Set<string>> = new Map(); // goes stale if addDirect is called aga
+
+  constructor() {
+    this.adjacencyList = new Map()
+
+    /**
+     * {
+     * "a": ["b", "c", "d"]
+     * "b": ["c", "d"]
+     * "c": ["e"]
+     * }
+     * 
+     * {
+     * "a": ["b", c ,d ,e]
+     * }
+     */
+  }
+
+  addDirect(className: string, directDependencies: Array<string>): void {
+    this.adjacencyList.set(className, new Set(directDependencies));
+    return;
+  }
+
+  findDeps(className: string): Set<string> {
+    if (this.mapFullDeps.has(className)) {
+      return this.mapFullDeps.get(className);
+    }
+
+    if (!this.adjacencyList.has(className)) return new Set()
+
+    const directDeps = this.adjacencyList.get(className);
+    
+    const m = []
+    directDeps.forEach(dep => {
+      m.push(this.findDeps(dep))
+    })
+    
+    const allDeps = m.reduce((acc, curr) => {
+      return this.concatSets(acc, curr)
+    }, new Set())
+
+    directDeps.forEach(d => {
+      allDeps.add(d);
+    })
+    
+    this.mapFullDeps.set(className, allDeps);
+
+    return allDeps;
+  }
+
+  dependenciesFor(className: string): Set<string> {
+    return this.findDeps(className)
+  }
+
+  private concatSets(set1: Set<string>, set2: Set<string>): Set<string> {
+    return new Set([...Array.from(set1), ...Array.from(set2)]);
+  }
+}
